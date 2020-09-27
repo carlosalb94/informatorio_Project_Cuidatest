ee36e7e63e9e55898325024c37db09e331937b51
Maxiperez7 <cristian.max91@gmail.com>
2020-09-27 19:56:29 -0300

    se agrego recuperacion de clave por correo

--- PROYECTO_INFO_g6/INFOg6/INFOg6/settings/base.py
+++ PROYECTO_INFO_g6/INFOg6/INFOg6/settings/base.py
@@ -124,3 +124,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS= True
+EMAIL_HOST_USER= 'cuidatest.covid@gmail.com'
+EMAIL_HOST_PASSWORD='Grupo006'
--- PROYECTO_INFO_g6/INFOg6/INFOg6/urls.py
+++ PROYECTO_INFO_g6/INFOg6/INFOg6/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth
+from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
 from . import views
 
 urlpatterns = [
@@ -9,6 +10,10 @@ urlpatterns = [
     path('', views.Home, name='home'),
     path('Login',auth.LoginView.as_view(template_name="users/login.html"),name="login"),
     path('Logout',auth.LogoutView.as_view(),name="logout"),
+    path('reset_password/',auth.PasswordResetView.as_view(template_name='users/password_reset_form.html'),name="reset_password"),
+    path('reset_password_sent/',auth.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name="password_reset_confirm"),
+    path('reset_password_complete/',auth.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name="password_reset_complete"),
     # URLS DE APLICACIONES
     path('Autotest/',include('apps.Autotest.urls')),
     path('users/', include('apps.users.urls')),
